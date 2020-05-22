63ec520c746424b22d4562c3b5ec759ec0f4e52b
Dylan <dylan@chaospott.de>
2020-05-22 21:57:40 +0200

    add gaps

--- config.h
+++ config.h
@@ -2,9 +2,10 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -12,10 +13,11 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_white[]       = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray2, col_gray1 },
 };
 
 /* tagging */
@@ -86,6 +88,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_l,      focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_h,      focusmon,       {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_Tab,    tagmon,         {.i = +1 } },
+
+        { MODKEY,                       XK_minus,  setgaps,        {.i = -1} },
+        { MODKEY,                       XK_plus,   setgaps,        {.i = +1} },
+        { MODKEY|ShiftMask,             XK_plus,   setgaps,        {.i = 0} },
 };
 
 /* button definitions */
