7c302c7d73cbb82c8d312ebef764a3c45ae82d07
Dylan <dylan@chaospott.de>
2020-07-04 17:05:41 +0200

    patch swallow.diff

--- config.h
+++ config.h
@@ -5,6 +5,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -24,14 +25,17 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const Rule *rules = {
-        /*
-         *	WM_CLASS(STRING) = instance, class
-         *	WM_NAME(STRING) = title
-         */
-        /* class      instance    title       tags mask     isfloating   monitor */
-        { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-        { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",     NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox",  NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "st",       NULL,     NULL,           0,         0,          1,          -1,        -1 },
+	{ "Alacritty",NULL,     NULL,           0,         0,          1,          -1,        -1 },
+	{ NULL,       NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
