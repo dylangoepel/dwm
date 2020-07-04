6f3b5347e8ae8ed68d4e87b4c087ac7a9f043b55
Dylan <dylan@chaospott.de>
2020-07-04 16:47:27 +0200

    slight changes

--- dwm.c
+++ dwm.c
@@ -2151,12 +2151,12 @@ main(int argc, char *argv[])
 		die("dwm: cannot open display");
 	checkotherwm();
 	setup();
-        system("sxhkd &");
-        system("dunst &");
 #ifdef __OpenBSD__
 	if (pledge("stdio rpath proc exec", NULL) == -1)
 		die("pledge");
 #endif /* __OpenBSD__ */
+        system("sxhkd &");
+        system("dunst &");
 	scan();
 	run();
 	cleanup();
